98b8574161f114c3ec262332df41ca295fdd9c23
Hiếu Vũ <85061650+kaiojudo@users.noreply.github.com>
2024-05-27 14:20:16 +0700

    . complete frontend

--- pokemon/src/components/Page3.jsx
+++ pokemon/src/components/Page3.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Page1() {
+export default function Page3() {
   return (
     <div className='hello-page'>
         <img src="./img1.png" alt=""/>
-        <h2 className='title'>What is FUDU1?</h2>
+        <h2 className='title'>What is FUDU3?</h2>
          <div className='description'>
             <p>$FUDU - native token on Ton Blockchain. Tired of seeing DUCKs on your timeline?Sick of seeing DUCKs using holders as exit liquidty?</p>
          </div>
